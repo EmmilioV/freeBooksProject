03bff446fb2970b256f42108928f41db918c3c0f
EmmilioV <68575969+EmmilioV@users.noreply.github.com>
2021-11-30 13:51:18 -0500

    annotation repository add

--- back/src/main/java/com/sofka/back/repositories/UserRepository.java
+++ back/src/main/java/com/sofka/back/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.sofka.back.repositories;
 
 import com.sofka.back.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
 }
