4eb1ffdff42196b565613df492f8fdceaac5dab4
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-30 22:30:34 -0500

    Se agregó estilo al home para los libros

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar />
-            <p>Soy el Home</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <div className="container mt-4">
+      <td><button className="btn btn-secondary mb-4">Agregar Nuevo libro</button></td>
+      <table class="table">
+        <thead>
+          <tr className="table-primary">
+            <th scope="col">ISBN</th>
+            <th scope="col">Titulo</th>
+            <th scope="col">Autor</th>
+            <th scope="col">Descripcion</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>Mark</td>
+            <td><button className="btn btn-success">Leer</button></td>
+            <td><button className="btn btn-primary">Editar</button></td>
+            <td><button className="btn btn-danger">Eliminar</button></td>
+          </tr>
+          
+        </tbody>
+      </table>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
