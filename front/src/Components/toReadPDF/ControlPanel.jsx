27838ed8cfc672ed19b01e9969d71791a53ad688
EmmilioV <emmiliov.p@gmail.com>
2021-12-03 03:39:00 -0500

    Diseño terminado, boton zoom añadido

--- front/src/Components/toReadPDF/ControlPanel.jsx
+++ front/src/Components/toReadPDF/ControlPanel.jsx
@@ -3,7 +3,7 @@ import '../../css/index.css'
 
 const ControlPanel = (props) => {
 
-    const{pageNumber, numPages, setPageNumber} = props;
+    const{pageNumber, numPages, setPageNumber, scale, setScale} = props;
 
     const isFirstPage = pageNumber === 1;
     const isLastPage = pageNumber === numPages;
@@ -29,24 +29,54 @@ const ControlPanel = (props) => {
         setPageNumber(Number(value));
     };
 
+    const isMinZoom = scale < 0.6;
+    const isMaxZoom = scale >= 3.0;
+
+    const zoomOutClass = isMinZoom ? 'disabled' : 'clickable';
+    const zoomInClass = isMaxZoom ? 'disabled' : 'clickable';
+
+    const zoomOut = () => {
+    if (!isMinZoom) setScale(scale - 0.1);
+    };
+
+    const zoomIn = () => {
+    if (!isMaxZoom) setScale(scale + 0.1);
+    };
 
     return ( 
-        <div className="control-panel m-3 p-3 d-flex align-items-baseline justify-content-center">
-            <i 
-                className={`fa fa-fast-backward mx-3 ${firstPageClass}`} 
-                onClick={goToFirstPage}/>
-            <i 
-                className={`fa fa-backward mx-3 ${firstPageClass}`} 
-                onClick={goToPreviousPage}/>
-            <span>
-                Page {pageNumber} of {numPages}
-            </span>
-            <i 
-                className={`fa fa-forward mx-3 ${lastPageClass}`} 
-                onClick={goToNextPage}/>
-            <i 
-                className={`fa fa-fast-forward mx-3 ${lastPageClass}`} 
-                onClick={goToLastPage}/>
+        <div className="control-panel m-3 p-3 d-flex align-items-baseline justify-content-between">
+            <div className="d-flex justify-contet-between align-items-baseline">
+                <i 
+                    className={`fa fa-fast-backward mx-3 ${firstPageClass}`} 
+                    onClick={goToFirstPage}
+                />
+                <i 
+                    className={`fa fa-backward mx-3 ${firstPageClass}`} 
+                    onClick={goToPreviousPage}
+                />
+                <span>
+                    Page <input name="pageNumber" type="number" min={1} max={numPages || 1} className="p-0 pl-1 mx-2 text-center" value={pageNumber} onChange={onPageChange}/> of {numPages}
+                </span>
+                <i 
+                    className={`fa fa-forward mx-3 ${lastPageClass}`} 
+                    onClick={goToNextPage}
+                />
+                <i 
+                    className={`fa fa-fast-forward mx-3 ${lastPageClass}`} 
+                    onClick={goToLastPage}
+                />
+            </div>
+            <div className="d-flex justify-content-between align-items-baseline">
+                <i
+                    className={`fa fa-search-minus mx-3 ${zoomOutClass}`}
+                    onClick={zoomOut}
+                />
+                <span>{(scale * 100).toFixed()}%</span>
+                <i
+                    className={`fa fa-search-plus mx-3 ${zoomInClass}`}
+                    onClick={zoomIn}
+                />
+            </div>
         </div>
     );
 };
--- front/src/Components/toReadPDF/PDFReader.jsx
+++ front/src/Components/toReadPDF/PDFReader.jsx
@@ -6,6 +6,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 const PDFReader = () => {
 
+    const [scale, setScale] = useState(1.0);
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
 
@@ -15,13 +16,13 @@ const PDFReader = () => {
 
     return ( 
         <div>
-            <section id="pdf-section" className="d-flex flex-column align-items-center w-100">
-                <ControlPanel numPages={numPages} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
+            <section id="pdf-section " className="d-flex flex-column align-items-center w-100">
+                <ControlPanel scale={scale} setScale={setScale} numPages={numPages} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
                 <Document
                     file="/assets/books/ciudad-de-hueso.pdf"
                     onLoadSuccess={onDocumentLoadSuccess}
                 >
-                    <Page pageNumber={pageNumber} />
+                    <Page pageNumber={pageNumber} scale={scale} />
                 </Document>
             </section>
         </div>
